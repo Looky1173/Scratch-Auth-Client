28cd2b8ddf192a76039dd3bd8fa2dc54e7487816
Looky1173 <60521810+Looky1173@users.noreply.github.com>
2022-05-28 14:02:21 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 export const sessionOptions = {
     password: process.env.SECRET_COOKIE_PASSWORD,
     cookieName: 'scratch-auth-token',
+    ttl: 90 * 24 * 60 * 60 // 90 days,
     cookieOptions: {
         secure: process.env.NODE_ENV === 'production',
     },
